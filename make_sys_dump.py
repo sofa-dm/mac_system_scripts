6eb81df2e0c5be72aaf9a28004bcbbbd8034ac77
Dzmitry Akulich <dzmitry_akulich1@epam.com>
2021-04-02 05:00:30 +0300

    no message

---
+++ make_sys_dump.py
@@ -0,0 +1,33 @@
+import os
+
+
+class FileManager():
+    def __init__(self, *kwargs):
+        for i, v in kwargs:
+            print(i, v)
+
+
+print('hello again')
+
+files_directories = {'zshrc': '', 'vimrc'}
+
+files = FileManager()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
