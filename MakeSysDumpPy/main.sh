c8b9dd9a9e4101273a232fcbd10e59ee7071856a
dm ak <akuulich@gmail.com>
2021-04-23 02:16:22 +0300

    12

--- MakeSysDumpPy/main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-chmod u+x /Users/dm/Dev/mac_system_scripts/MakeSysDump/make_sys_dump.py
-
-sudo python /Users/dm/Dev/mac_system_scripts/MakeSysDump/make_sys_dump.py
-
-echo Done. Press RETURN to exit
-read press
-
